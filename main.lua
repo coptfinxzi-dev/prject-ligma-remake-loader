1e3ada17840985e553e945c4fe847a64d3c9e493
coptfinxzi-dev <coptfinxzi@gmail.com>
2026-02-19 15:02:03 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,4 @@
---[=[
- d888b  db    db d888888b      .d888b.      db      db    db  .d8b.  
+--=[ d888b  db    db d888888b      .d888b.      db      db    db  .d8b.  
 88' Y8b 88    88   `88'        VP  `8D      88      88    88 d8' `8b 
 88      88    88    88            odD'      88      88    88 88ooo88 
 88  ooo 88    88    88          .88'        88      88    88 88~~~88 
@@ -11,10 +10,9 @@
 local G2L = {};
 
 -- StarterGui.ligamremakefynxzi
-G2L["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
+G2L["1"] = Instance.new("ScreenGui");
 G2L["1"]["Name"] = [[ligamremakefynxzi]];
 G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
-
 
 -- StarterGui.ligamremakefynxzi.ImageLabel
 G2L["2"] = Instance.new("ImageLabel", G2L["1"]);
@@ -26,16 +24,12 @@ G2L["2"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
 G2L["2"]["BackgroundTransparency"] = 1;
 G2L["2"]["Position"] = UDim2.new(0.45241, 0, 0.40799, 0);
 
-
 -- StarterGui.ligamremakefynxzi.ImageLabel.UICorner
 G2L["3"] = Instance.new("UICorner", G2L["2"]);
 G2L["3"]["CornerRadius"] = UDim.new(1, 0);
 
-
 -- StarterGui.ligamremakefynxzi.ImageLabel.LocalScript
 G2L["4"] = Instance.new("LocalScript", G2L["2"]);
-
-
 
 -- StarterGui.ligamremakefynxzi.ImageLabel.Frame
 G2L["5"] = Instance.new("Frame", G2L["2"]);
@@ -45,7 +39,6 @@ G2L["5"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
 G2L["5"]["Size"] = UDim2.new(0, 209, 0, 72);
 G2L["5"]["Position"] = UDim2.new(0.99518, 0, 0.22324, 0);
 G2L["5"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
-
 
 -- StarterGui.ligamremakefynxzi.ImageLabel.Frame.TextLabel
 G2L["6"] = Instance.new("TextLabel", G2L["5"]);
@@ -63,10 +56,9 @@ G2L["6"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
 G2L["6"]["Text"] = [[project ligma 
 remake by fynxzi]];
 
-
 -- StarterGui.ligamremakefynxzi.ImageLabel.LocalScript
 local function C_4()
-local script = G2L["4"];
+	local script = G2L["4"];
 	local logo = script.Parent
 	local TweenService = game:GetService("TweenService")
 	
@@ -86,4 +78,5 @@ local script = G2L["4"];
 end;
 task.spawn(C_4);
 
-return G2L["1"], require;
+-- Přesun do StarterGui místo přímého vložení do PlayerGui
+G2L["1"].Parent = game:GetService("StarterGui")
